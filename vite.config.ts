7dd2af0c6e57ee6d84fd868118ee7db4b5f332ab
benzabihi <112029552+benzabihi@users.noreply.github.com>
2025-06-14 19:55:09 -0400

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/benzabihi.io',
+  base: '/',
+  build:{outDir: 'docs',},
   plugins: [react(), tailwindcss()],
 })
